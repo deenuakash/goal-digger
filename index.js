e002f6fd4c83cb5e16f0b649cd8bcdc797afb122
deenu <akashraja1108@gmail.com>
2024-09-25 21:03:52 +0530

    Add user Routes
    Added session based authentication with cookies
    Setup user model with signup and signin routes
    Performed Zod validation
    Configured MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 import express from "express";
-require("dotenv").config;
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import store from "./config/dbStore.js";
+import "./config/db.js";
+import userRouter from "./routes/userRouter.js";
 
 const app = express();
 
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+    store,
+  })
+);
+
+app.use("/api/user", userRouter);
+
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => `Listening on port ${port}`);
+app.listen(port, () => console.log(`Listening on port ${port}`));
