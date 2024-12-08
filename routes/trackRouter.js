71d0e93014bf6fd6e80bb8412502cc50bbddc291
deenu <akashraja1108@gmail.com>
2024-12-08 13:01:31 +0530

    Create auth middleware

--- routes/trackRouter.js
+++ routes/trackRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { setTrack } from "../controllers/trackController.js";
+import { isAuthenticated } from "../middlewares/isAuthenticated.js";
 
 const trackRouter = Router();
 
-trackRouter.post("/post", setTrack);
+trackRouter.post("/post", isAuthenticated, setTrack);
 
 export default trackRouter;
