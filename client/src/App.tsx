26c6b2f7da3acbed70648a1789332ce72ffaaca6
deenu <akashraja1108@gmail.com>
2024-12-24 20:34:50 +0530

    Setup pages and routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home, Layout, Track } from "./pages";
+
 function App() {
-  return (
-    <>
-      <h1 className="text-2xl font-black">Brain Trail</h1>
-    </>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/track",
+          element: <Track />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
